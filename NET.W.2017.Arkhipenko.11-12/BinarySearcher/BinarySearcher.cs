f1409d704d4289d78b44d29bb80c1140b0d98e40
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-11-15 14:36:51 +0300

    Some changes

--- NET.W.2017.Arkhipenko.11-12/BinarySearcher/BinarySearcher.cs
+++ NET.W.2017.Arkhipenko.11-12/BinarySearcher/BinarySearcher.cs
@@ -1,30 +1,11 @@
 ﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace BinarySearcher
 {
-    public class Comparer : IComparer<int>
-    {
-        /// <summary>
-        /// Method compares two elements
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns>0, if elements are equal; 1, if x is greater then y, and -1, if  x is smaller then y </returns>
-        public int Compare(int x, int y)
-        {
-            if (x == y)
-            {
-                return 0;
-            }
-            if (x > y)
-            {
-                return 1;
-            }
-            return -1;
-        }
-    }
+   
     public static class BinarySearcher 
     {
         /// <summary>
@@ -35,6 +16,14 @@ namespace BinarySearcher
         /// <returns>true, if the array contains a value, and false, if is not</returns>
         public static bool BinarySearch(this int[] array, int value)
         {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(nameof(array));
+            }
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
             int bottom = 0;
             int top = array.Length - 1;
             int middle = top >> 1;
@@ -67,6 +56,14 @@ namespace BinarySearcher
         /// <returns>true, if the array contains a value, and false, if is not</returns>
         public static bool BinarySearch<T>(T item, T[] array, IComparer<T> comparer)
         {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(nameof(array));
+            }
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
             uint bottom = 0;
             uint top = (uint)array.Length - 1;
             uint middle = top >> 1;
@@ -100,6 +97,14 @@ namespace BinarySearcher
         /// <returns>true, if the array contains a value, and false, if is not</returns>
         public static bool BinarySearchThanksTooArrayList<T>(T item, T[] array)
         {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(nameof(array));
+            }
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
             ArrayList list = new ArrayList();
          
             for (int i = 0; i < array.Length; i++)
---
+++ NET.W.2017.Arkhipenko.11-12/BinarySearcherTest/Comparer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace BinarySearcherTest
+{
+    public class Comparer : IComparer<int>
+    {
+        /// <summary>
+        /// Method compares two elements
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>0, if elements are equal; 1, if x is greater then y, and -1, if  x is smaller then y </returns>
+        public int Compare(int x, int y)
+        {
+            if (x == y)
+            {
+                return 0;
+            }
+            if (x > y)
+            {
+                return 1;
+            }
+            return -1;
+        }
+    }
+}
