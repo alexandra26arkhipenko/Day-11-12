a9f8d60a6b48215e3f012cd12fdf43780f91f543
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-11-15 11:30:26 +0300

    Fibinacci numbers

---
+++ NET.W.2017.Arkhipenko.11-12/Fibinacci/Fibinacci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibinacci
+{
+    public class Fibinacci
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<long> FibonacciGenerate()
+        {
+            long lhs = 0, rhs = 1;
+
+            yield return 0;
+            yield return 1;
+            while (true)
+            {
+                long tmp = lhs + rhs;
+                lhs = rhs;
+                rhs = tmp;
+                yield return rhs;
+            }
+        }
+    }
+}
